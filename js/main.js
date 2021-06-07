8bd1a3f95339e4f48ef98e65699466584313f86d
StasyaShu <stanislava.semenova@gmail.com>
2021-06-07 20:54:17 +0300

    ДЗ № 4 выполнено

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomInteger (min, max) {
+function getRandomInteger(min, max) {
 
   if (min < 0 || max < 0) {
     throw new Error('getRandomInteger — аргументы могут быть только положительными числами');
@@ -8,9 +8,9 @@ function getRandomInteger (min, max) {
   const rand = Math.random() * (max - min + 1) + min;
   return Math.floor(rand);
 }
-getRandomInteger (0, 10);
+getRandomInteger(0, 10);
 
-function getRandomFloatInteger (min, max, digits) {
+function getRandomFloatInteger(min, max, digits) {
 
   if (min < 0 || max < 0 || digits < 0) {
     throw new Error('getRandomFloatInteger — аргументы могут быть только положительными числами');
@@ -18,7 +18,7 @@ function getRandomFloatInteger (min, max, digits) {
   const number = Math.random() * (max - min) + min;
   return +number.toFixed(digits);
 }
-getRandomFloatInteger (5, 9, 2);
+getRandomFloatInteger(5, 9, 2);
 
 // напишите необходимые функции для создания массива из 10 сгенерированных JS-объектов.
 // Каждый объект массива — описание похожего объявления неподалёку.
@@ -52,18 +52,34 @@ getRandomFloatInteger (5, 9, 2);
 // lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
 
 Array.prototype.getRandomItem = function () {
-  let item = Math.floor(Math.random() * this.length);
-  return this[item];
+  const ITEM = Math.floor(Math.random() * this.length);
+  return this[ITEM];
+};
+
+function shuffle(array) {
+  let mm = array.length, tt, ii;
+  while (mm) {
+    ii = Math.floor(Math.random() * mm--);
+    tt = array[mm];
+    array[mm] = array[ii];
+    array[ii] = tt;
+  }
+  return array;
 }
 
-let allFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner',];
-const createArr = ([...source], maxLength) => Array.from(
-  { length: Math.min(source.length, 1 + Math.random() * maxLength | 0) },
-  () => source.splice(Math.random() * source.length | 0, 1)[0]
-);
-const [arr1, arr2, arr3 ] = [...Array(3)].map(() => createArr(allFeatures, allFeatures.length+1));
+const allFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const allPhotos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const housingType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkinArray = ['12:00', '13:00', '14:00'];
+const checkoutArray = ['12:00', '13:00', '14:00'];
+const newArrayFeatures = shuffle(allFeatures);
+const newArrayPhotos = shuffle(allPhotos);
 
-let avatars = [
+const avatars = [
   'img/avatars/user01.png',
   'img/avatars/user02.png',
   'img/avatars/user03.png',
@@ -72,25 +88,65 @@ let avatars = [
   'img/avatars/user06.png',
   'img/avatars/user07.png',
   'img/avatars/user08.png',
-]
-
-let lat = getRandomFloatInteger(35.65000, 35.70000, 5);
-let lng = getRandomFloatInteger(139.70000, 139.80000, 5);
+];
 
 const ANNOUNCEMENT = {
   author: {
-    avatar: avatars.getRandomItem()
-  }
-}
+    avatar: avatars.getRandomItem(),
+  },
 
-const OFFER = {
-  title: 'Сдаю в аренду квартиру',
-  address: '{{location.x}}, {{location.y}}',
-  price: getRandomInteger(1000, 1000000),
-  type: 'flat',
-  rooms: getRandomInteger(1, 3),
-  guests: getRandomInteger(1, 5),
-  checkin: '14:00',
-  checkout: '12:00',
-  features: [arr1, arr2, arr3],
+  offer: {
+    title: 'Аренда жилья в Токио',
+    address: '{{location.x}}, {{location.y}}',
+    price: getRandomInteger(1000, 10000),
+    type: housingType.getRandomItem(),
+    rooms: getRandomInteger(1, 3),
+    guests: getRandomInteger(1, 5),
+    checkin: checkinArray.getRandomItem(),
+    checkout: checkoutArray.getRandomItem(),
+    features: newArrayFeatures.slice(0, getRandomInteger(1, allFeatures.length)),
+    description: 'Чистое, светлое помещение для комфортного проживания с отличной инфраструктурой.',
+    photos: newArrayPhotos.slice(0, getRandomInteger(1, allPhotos.length)),
+  },
+
+  location: {
+    lat: getRandomFloatInteger(35.65000, 35.70000, 5),
+    lng: getRandomFloatInteger(139.70000, 139.80000, 5),
+  },
+};
+ANNOUNCEMENT;
+
+function getAnnouncementArray (amount) {
+  const announcementArray = [];
+  for (let ii = 1; ii <= amount; ii++) {
+    announcementArray.push({
+      author: {
+        avatar: avatars.getRandomItem(),
+      },
+
+      offer: {
+        title: 'Аренда жилья в Токио',
+        address: '{{location.x}}, {{location.y}}',
+        price: getRandomInteger(1000, 10000),
+        type: housingType.getRandomItem(),
+        rooms: getRandomInteger(1, 3),
+        guests: getRandomInteger(1, 5),
+        checkin: checkinArray.getRandomItem(),
+        checkout: checkoutArray.getRandomItem(),
+        features: newArrayFeatures.slice(0, getRandomInteger(1, allFeatures.length)),
+        description: 'Чистое, светлое помещение для комфортного проживания с отличной инфраструктурой.',
+        photos: newArrayPhotos.slice(0, getRandomInteger(1, allPhotos.length)),
+      },
+
+      location: {
+        lat: getRandomFloatInteger(35.65000, 35.70000, 5),
+        lng: getRandomFloatInteger(139.70000, 139.80000, 5),
+      },
+    });
+  }
+  return announcementArray;
 }
+announcementArray;
+getAnnouncementArray(10);
+
+
