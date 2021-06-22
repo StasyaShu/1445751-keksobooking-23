57ebea29bbceca3d30f9675a4821def6eece3443
StasyaShu <stanislava.semenova@gmail.com>
2021-06-22 13:21:09 +0300

    Корректировка активное и неактивное состояние

--- js/form.js
+++ js/form.js
@@ -1,54 +1,20 @@
+const forms = document.querySelectorAll('form');
 const searchForm = document.querySelector('.ad-form');
-const serchFormInputs = searchForm.querySelectorAll('input');
-const serchFormSelects = searchForm.querySelectorAll('select');
-const searchFormTextArea = searchForm.querySelector('#description');
-
 const mapForm = document.querySelector('.map__filters');
-const mapFormInputs = mapForm.querySelectorAll('input');
-const mapFormSelects = mapForm.querySelectorAll('select');
 
-const buttonsInForm = document.querySelectorAll('button');
-
-const deactivateForm = () => {
-  searchForm.classList.add('.ad-form--disabled');
-  serchFormInputs.forEach((value) => {
-    value.disabled = true;
-  });
-  serchFormSelects.forEach((value) => {
-    value.disabled = true;
-  });
-  searchFormTextArea.disabled = true;
-  mapForm.classList.add('.map__filters--disabled');
-  mapFormInputs.forEach((value) => {
-    value.disabled = true;
-  });
-  mapFormSelects.forEach((value) => {
-    value.disabled = true;
-  });
-  buttonsInForm.forEach((value) => {
-    value.disabled = true;
+const toggleDisabledOnFormNodes = (isDisabled) => {
+  forms.forEach((value) => {
+    for (const element of value.elements) {
+      element.disabled = isDisabled;
+    }
   });
 };
 
-const activateForm = () => {
-  searchForm.classList.remove('.ad-form--disabled');
-  serchFormInputs.forEach((value) => {
-    value.disabled = false;
-  });
-  serchFormSelects.forEach((value) => {
-    value.disabled = false;
-  });
-  searchFormTextArea.disabled = false;
-  mapForm.classList.remove('.map__filters--disabled');
-  mapFormInputs.forEach((value) => {
-    value.disabled = false;
-  });
-  mapFormSelects.forEach((value) => {
-    value.disabled = false;
-  });
-  buttonsInForm.forEach((value) => {
-    value.disabled = false;
-  });
+const togglePageActiveState = (isDisabled) => {
+  searchForm.classList.toggle('ad-form--disabled', isDisabled);
+  mapForm.classList.toggle('map__filters--disabled', isDisabled);
+
+  toggleDisabledOnFormNodes(isDisabled);
 };
 
-export {deactivateForm, activateForm};
+export {togglePageActiveState};
--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 import {generateAds} from './render.js';
 import {offersArray} from './offers-array.js';
-import {deactivateForm, activateForm} from './form.js';
+import {togglePageActiveState} from './form.js';
 
 const similarOffer = document.querySelector('#map-canvas');
 
 generateAds(offersArray);
 similarOffer.appendChild(generateAds(offersArray));
 
-deactivateForm();
-activateForm();
+togglePageActiveState(true);
